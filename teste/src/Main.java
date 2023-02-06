b87ca105cb645a8a38c90fe5e13c1be5a42f0b3b
Felipe Dantas <dantas.contatod@gmail.com>
2023-02-06 09:18:19 -0300

    first-leasson

---
+++ teste/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+int x = 10;
+int y = 20;
+int soma = x + y;
+        System.out.println(soma);
+    }
+}

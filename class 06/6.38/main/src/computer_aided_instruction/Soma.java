e104cdcd89a9fadab1ea788940026afc1dca3779
Felipe Dantas <dantas.contatod@gmail.com>
2023-03-27 08:52:28 -0300

    adding overload

---
+++ class 06/6.38/main/src/computer_aided_instruction/Soma.java
@@ -0,0 +1,14 @@
+package computer_aided_instruction;
+
+public class Soma {
+
+   //Overload
+    public int soma(int a, int b) {
+        return a + b;
+    }
+
+    public double soma(double a, double b) {
+        return a + b;
+    }
+    
+}
